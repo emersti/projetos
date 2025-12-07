7fee0b84e8d34b22355d9d6f194ea7ef6866821a
emersti <stigliani@alumni.usp.br>
2025-12-06 23:47:32 -0300

    Alterando arquivos urls.py de projeto_teste e base.html de templates para aparecer o favicon no resultado de pequisa do Google

--- projeto_teste/urls.py
+++ projeto_teste/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('consulta_risco.urls')),
+    # Favicon na raiz - importante para Google e navegadores
+    # O Google busca o favicon em /favicon.ico na raiz do domínio
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'), permanent=True)),
 ]
 
 
